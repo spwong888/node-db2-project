5d261c68b96cc8e438ae0293b17fc8ffe72fb58f
Spencer Wong <sawong887@gmail.com>
2023-12-30 21:25:43 -0800

    newer version

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,35 @@
-// DO YOUR MAGIC
+const express = require('express');
+const carsRouter = require('./cars-router');
+const Car = require('./cars-model');
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require('./cars-middleware');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json('posting new car');
+  });
+
+  router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) => {
+    try {
+      const newCar = await Car.create(req.body);
+      res.json(newCar); 
+    } catch (err) {
+      next(err);
+    }
+  });
+
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+const cars = [
+    {
+        vin: 'JF1GC2444PG510518',
+        make: 'Toyota',
+        model: 'Camry',
+        mileage: 1000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: 'WDDGF5HB4DA828865',
+        make: 'Toyota',
+        model: 'Prius',
+        mileage: 3000,
+        title: 'salvage',
+        transmission: 'manual'
+    },
+    {
+        vin: '2C8GP54L95R123478',
+        make: 'Ford',
+        model: 'Focus',
+        mileage: 5000,
+        title: 'clean',
+    }
+];
+
+    exports.seed = function(knex) {
+        return knex('cars')
+        .truncate().then(() => {
+            knex('cars').insert(cars)
+    })
+}
